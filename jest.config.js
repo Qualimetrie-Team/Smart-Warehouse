7d3b3f1c26e36c1e13297282cc9141ffa5b3babc
Maria Rakoto <maria.rakotonirinas@gmail.com>
2026-05-27 22:18:59 +0300

    Sppressioon test

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverageFrom: [
-    'src/**/*.js',
-    'routes/**/*.js'
-  ],
-  coverageReporters: ['lcov', 'text', 'clover'],
-  coverageDirectory: 'coverage'
-};
